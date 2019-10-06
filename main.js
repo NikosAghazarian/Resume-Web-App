d16d9d4345ecf017ee212b257630e22fd25b3fab
Nikos Aghazarian <nikosaghazarian@gmail.com>
2019-10-05 18:19:42 -0700

    more fixes

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ const viewRouter = require('./UI/routes/ViewRouter.js');
 
 
 // Asset Setup
-app.use('/favicon.ico', express.static('Assets/favicon.ico'));
-app.use('/Resume', express.static('Assets/Nikos_Aghazarian_Resume.pdf'));
-app.use('/Assets/background.jpg', express.static('Assets/background.jpg'));
-app.use('/Assets/portrait_cropped.jpg', express.static('Assets/portrait_cropped.jpg'));
+app.use('/favicon.ico', express.static('/Assets/favicon.ico'));
+app.use('/Resume', express.static('/Assets/Nikos_Aghazarian_Resume.pdf'));
+app.use('/Assets/background.jpg', express.static('/Assets/background.jpg'));
+app.use('/Assets/portrait_cropped.jpg', express.static('/Assets/portrait_cropped.jpg'));
 
 //Static file setup
 app.use(express.static(__dirname + '/UI/'));
