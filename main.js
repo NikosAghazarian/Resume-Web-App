576091b32fea9a785f47c9d75ad5fde0505d1230
Nikos Aghazarian <nikosaghazarian@gmail.com>
2019-11-17 21:52:51 -0800

    Added images to projects that had them.

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const viewRouter = require('./UI/routes/ViewRouter.js');
 // Asset Setup
 app.use('/Assets/background.jpg', express.static('Assets/background.jpg'));
 app.use('/Assets/portrait_cropped.jpg', express.static('Assets/portrait_cropped.jpg'));
+app.use('/Assets/Waveform_App.PNG', express.static('Assets/Waveform_App.PNG'));
+app.use('/Assets/Connect_4.PNG', express.static('Assets/Connect_4.PNG'));
+app.use('/Assets/InternetMonitor.PNG', express.static('Assets/InternetMonitor.PNG'));
+app.use('/Assets/Sql.PNG', express.static('Assets/Sql.PNG'));
 
 //Static file setup
 app.use(express.static(__dirname + '/UI/'));
@@ -16,5 +20,5 @@ app.use('/', viewRouter.index);
 
 
 // hosting http server
-let port = process.env.PORT;
+let port = 3000;//process.env.PORT;
 app.listen(port, console.log('NodeJS/Express server started on: ' + port));
