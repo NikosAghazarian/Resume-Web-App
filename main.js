850cf880a66489053267ec33275d8ba7ff5dfb9e
Nikos Aghazarian <nikosaghazarian@gmail.com>
2019-11-17 21:54:44 -0800

    woops, pushed localport

--- main.js
+++ main.js
@@ -20,5 +20,5 @@ app.use('/', viewRouter.index);
 
 
 // hosting http server
-let port = 3000;//process.env.PORT;
+let port = process.env.PORT;
 app.listen(port, console.log('NodeJS/Express server started on: ' + port));
